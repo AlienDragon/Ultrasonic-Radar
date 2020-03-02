7866550356cf7526f4347d4dcb22ecfaa45191c4
OliverPC <oliver.simon333@gmail.com>
2020-03-02 21:45:42 +0000

    Initial commit

---
+++ RadarDisplay/RadarDisplay/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RadarDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInit = new System.Windows.Forms.Button();
+            this.cbCom = new System.Windows.Forms.ComboBox();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(13, 13);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(102, 43);
+            this.btnInit.TabIndex = 0;
+            this.btnInit.Text = "Initialise";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // cbCom
+            // 
+            this.cbCom.FormattingEnabled = true;
+            this.cbCom.Location = new System.Drawing.Point(121, 13);
+            this.cbCom.Name = "cbCom";
+            this.cbCom.Size = new System.Drawing.Size(121, 21);
+            this.cbCom.TabIndex = 2;
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(13, 63);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(30, 13);
+            this.lblTest.TabIndex = 3;
+            this.lblTest.Text = "temp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.cbCom);
+            this.Controls.Add(this.btnInit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.ComboBox cbCom;
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
---
+++ RadarDisplay/RadarDisplay/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace RadarDisplay
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort comPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            for(int i = 0; i < ports.Count() - 1; i++)
+            {
+                cbCom.Items.Add(ports[i]);
+            }
+
+            cbCom.Items.Add("Test");
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            if(cbCom.SelectedItem != null)
+            {
+                try
+                {
+                    comPort = new SerialPort();
+                    comPort.BaudRate = 9600;
+                    comPort.PortName = cbCom.SelectedItem.ToString();
+                    comPort.Open();
+
+                    btnInit.Enabled = false;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("There has been an error connecting");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Please select an item from the drop down");
+            }
+        }
+    }
+}
