71f8f1cb0ad977acf9bf5ce1adbc98803b933ac9
OliverPC <oliver.simon333@gmail.com>
2020-03-02 22:01:02 +0000

    Added close button and group boxes for formatting

--- RadarDisplay/RadarDisplay/Form1.Designer.cs
+++ RadarDisplay/RadarDisplay/Form1.Designer.cs
@@ -30,12 +30,18 @@
         {
             this.btnInit = new System.Windows.Forms.Button();
             this.cbCom = new System.Windows.Forms.ComboBox();
-            this.lblTest = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lbDataView = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnInit
             // 
-            this.btnInit.Location = new System.Drawing.Point(13, 13);
+            this.btnInit.Location = new System.Drawing.Point(6, 46);
             this.btnInit.Name = "btnInit";
             this.btnInit.Size = new System.Drawing.Size(102, 43);
             this.btnInit.TabIndex = 0;
@@ -45,34 +51,77 @@
             // 
             // cbCom
             // 
+            this.cbCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbCom.FormattingEnabled = true;
-            this.cbCom.Location = new System.Drawing.Point(121, 13);
+            this.cbCom.Location = new System.Drawing.Point(6, 19);
             this.cbCom.Name = "cbCom";
             this.cbCom.Size = new System.Drawing.Size(121, 21);
             this.cbCom.TabIndex = 2;
             // 
-            // lblTest
+            // groupBox1
             // 
-            this.lblTest.AutoSize = true;
-            this.lblTest.Location = new System.Drawing.Point(13, 63);
-            this.lblTest.Name = "lblTest";
-            this.lblTest.Size = new System.Drawing.Size(30, 13);
-            this.lblTest.TabIndex = 3;
-            this.lblTest.Text = "temp";
+            this.groupBox1.Controls.Add(this.btnEnd);
+            this.groupBox1.Controls.Add(this.btnInit);
+            this.groupBox1.Controls.Add(this.cbCom);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Start prog";
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Enabled = false;
+            this.btnEnd.Location = new System.Drawing.Point(115, 47);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(50, 42);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "Stop";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbDataView);
+            this.groupBox2.Location = new System.Drawing.Point(12, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(268, 301);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(304, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(462, 407);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Graphic Display";
+            // 
+            // lbDataView
+            // 
+            this.lbDataView.FormattingEnabled = true;
+            this.lbDataView.Location = new System.Drawing.Point(7, 20);
+            this.lbDataView.Name = "lbDataView";
+            this.lbDataView.Size = new System.Drawing.Size(255, 95);
+            this.lbDataView.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblTest);
-            this.Controls.Add(this.cbCom);
-            this.Controls.Add(this.btnInit);
+            this.ClientSize = new System.Drawing.Size(778, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -80,7 +129,11 @@
 
         private System.Windows.Forms.Button btnInit;
         private System.Windows.Forms.ComboBox cbCom;
-        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox lbDataView;
     }
 }
 
--- RadarDisplay/RadarDisplay/Form1.cs
+++ RadarDisplay/RadarDisplay/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Diagnostics;
 
 namespace RadarDisplay
 {
@@ -23,12 +24,10 @@ namespace RadarDisplay
         private void Form1_Load(object sender, EventArgs e)
         {
             string[] ports = SerialPort.GetPortNames();
-            for(int i = 0; i < ports.Count() - 1; i++)
+            for(int i = 0; i < ports.Count(); i++)
             {
                 cbCom.Items.Add(ports[i]);
             }
-
-            cbCom.Items.Add("Test");
         }
 
         private void btnInit_Click(object sender, EventArgs e)
@@ -40,9 +39,12 @@ namespace RadarDisplay
                     comPort = new SerialPort();
                     comPort.BaudRate = 9600;
                     comPort.PortName = cbCom.SelectedItem.ToString();
+                    comPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
                     comPort.Open();
 
                     btnInit.Enabled = false;
+                    btnEnd.Enabled = true;
+                    cbCom.Enabled = false;
                 }
                 catch (Exception)
                 {
@@ -54,6 +56,23 @@ namespace RadarDisplay
             {
                 MessageBox.Show("Please select an item from the drop down");
             }
+        }
+
+
+        //Gets called whenever data is received through the com port
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Debug.Print(indata);
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            comPort.Close();
+            btnInit.Enabled = true;
+            cbCom.Enabled = true;
+            btnEnd.Enabled = false;
         }
     }
 }
